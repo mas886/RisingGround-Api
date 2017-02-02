170bfeca9a5854d13c3a6249863d4a7c464a662c
mas886 <thealchemist886@gmail.com>
2017-02-02 11:57:16 +0100

    Refactored user_id to userId, added getUserIdByToken, added private cleanTokensById.

--- class/Token.php
+++ class/Token.php
@@ -28,20 +28,27 @@ class Token {
             return 0;
         }
     }
-
+    
     private function cleanTokens($user) {
-        //This will clean expired user tokens
-        //
+        //This will clean expired user tokens by Username
         $connection = connect();
-        $sql = "DELETE FROM `user_login_tokens` WHERE `expireDate`< CURRENT_TIME AND `user_id` = (SELECT `id` FROM `user` WHERE name=:user)";
+        $sql = "DELETE FROM `user_login_tokens` WHERE `expireDate`< CURRENT_TIME AND `userId` = (SELECT `id` FROM `user` WHERE name=:user)";
         $sth = $connection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
         $sth->execute(array(':user' => $user));
+    }
+    
+    private function cleanTokensById($userId) {
+        //This will clean expired user tokens by Username
+        $connection = connect();
+        $sql = "DELETE FROM `user_login_tokens` WHERE `expireDate`< CURRENT_TIME AND `userId` = :userId";
+        $sth = $connection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute(array(':userId' => $userId));
     }
 
     private function insertTokenIntoDB($user, $token) {
         //Inserts the token into the db with an expire date of +30 days
         $connection = connect();
-        $sql = "INSERT INTO `user_login_tokens`(`user_id`, `token`, `expireDate`) VALUES ((SELECT `id` FROM `user` WHERE name=:user),:token,DATE_ADD(CURRENT_TIME,INTERVAL 30 DAY))";
+        $sql = "INSERT INTO `user_login_tokens`(`userId`, `token`, `expireDate`) VALUES ((SELECT `id` FROM `user` WHERE name=:user),:token,DATE_ADD(CURRENT_TIME,INTERVAL 30 DAY))";
         $sth = $connection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
         $sth->execute(array(':user' => $user, ':token' => $token));
     }
@@ -58,6 +65,26 @@ class Token {
             return True;
         } else {
             return False;
+        }
+    }
+    
+    function getUserIdByToken($token){
+        $connection = connect();
+        $sql = "SELECT `userId`,`expireDate` FROM `user_login_tokens` WHERE `token`=:token";
+        $tokendb = [];
+        $stmt = $connection->prepare($sql);
+        if ($stmt->execute([':token' => $token])) {
+            $tokendb = $stmt->fetch();
+        }
+        if (sizeof($tokendb) > 1) {
+            if (date('Y/m/d h:i:s', strtotime($tokendb['expireDate']))>date('Y/m/d h:i:s', time())){
+                return $tokendb['userId'];
+            }else{
+                $this->cleanTokensById($tokendb['userId']);
+                return "Expired";
+            }
+        } else {
+            return 0;
         }
     }
 
