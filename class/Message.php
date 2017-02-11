01415b324429a22c0d668d20310f27b69e61d2e4
mas886 <thealchemist886@gmail.com>
2017-02-11 13:28:14 +0100

    Moved queries from Message.php to MessageDAO.php

--- class/Message.php
+++ class/Message.php
@@ -7,6 +7,7 @@
 
 include_once("Token.php");
 include_once("User.php");
+include_once("./db/MessageDAO.php");
 
 class Message {
 
@@ -23,19 +24,12 @@ class Message {
             $usr = new User();
             //We check if the reciever exist
             if ($usr->exist($receiver)) {
-                return $this->insertMessageIntoDb($tokenOwner, $receiver, $text);
+                $dao=new MessageDAO;
+                return $dao->insertMessageIntoDb($tokenOwner, $receiver, $text);
             } else {
                 return "User doesn't exist";
             }
         }
-    }
-
-    private function insertMessageIntoDb($userId, $receiver, $text) {
-        $connection = connect();
-        $sql = "INSERT INTO `user_inbox`(`userSendId`, `userReceiveId`, `content`) VALUES (:userId,(SELECT `id` FROM `user` WHERE name=:receiver),:text)";
-        $sth = $connection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-        $sth->execute(array(':userId' => $userId, ':receiver' => $receiver, ':text' => $text));
-        return 1;
     }
 
     function getMessages($token) {
@@ -48,19 +42,11 @@ class Message {
                 return $tokenOwner;
             }
             //If the token is correct we will continue
-            return $this->getMessagesById($tokenOwner);
+            $dao=new MessageDAO;
+            return $dao->getMessagesById($tokenOwner);
         }
     }
 
-    private function getMessagesById($userId) {
-        $connection = connect();
-        $sql = 'SELECT (SELECT `name`FROM `user`WHERE `id`= `userSendId`) as`from`, `id`, `sendDate`, `content` FROM `user_inbox` WHERE `userReceiveId`=:userId';
-        $sth = $connection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-        $sth->execute(array(':userId' => $userId));
-        $messages = $sth->fetchAll(PDO::FETCH_ASSOC);
-        return $messages;
-    }
-    
     function deleteMessage($token, $messageId){
         if (strlen($token) != 30 || strlen($messageId)==0) {
             return 0;
@@ -71,20 +57,8 @@ class Message {
                 return $tokenOwner;
             }
             //If the token is correct we will continue
-            return $this->deleteMessageFromDb($tokenOwner, $messageId);
-        }
-    }
-    
-    private function deleteMessageFromDb($tokenOwner, $messageId){
-        $connection = connect();
-        $sql = "DELETE FROM `user_inbox` WHERE `id`= :messageId AND `userReceiveId`=:userId";
-        $sth = $connection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-        $sth->execute(array(':messageId' => $messageId, ':userId' => $tokenOwner));
-        //If any row was affected will return success code (1)
-        if($sth->rowCount()!=0){
-            return 1;
-        }else{
-            return "Message does not exist";
+            $dao=new MessageDAO;
+            return $dao->deleteMessageFromDb($tokenOwner, $messageId);
         }
     }
 
---
+++ db/MessageDAO.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Queries to interact with user_inbox table
+ *
+ * @author redrednose
+ */
+
+include_once("./class/config.php");
+
+class MessageDAO {
+
+    function insertMessageIntoDb($userId, $receiver, $text) {
+        $connection = connect();
+        $sql = "INSERT INTO `user_inbox`(`userSendId`, `userReceiveId`, `content`) VALUES (:userId,(SELECT `id` FROM `user` WHERE name=:receiver),:text)";
+        $sth = $connection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute(array(':userId' => $userId, ':receiver' => $receiver, ':text' => $text));
+        return 1;
+    }
+    
+    function getMessagesById($userId) {
+        $connection = connect();
+        $sql = 'SELECT (SELECT `name`FROM `user`WHERE `id`= `userSendId`) as`from`, `id`, `sendDate`, `content` FROM `user_inbox` WHERE `userReceiveId`=:userId';
+        $sth = $connection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute(array(':userId' => $userId));
+        $messages = $sth->fetchAll(PDO::FETCH_ASSOC);
+        return $messages;
+    }
+    
+    function deleteMessageFromDb($tokenOwner, $messageId){
+        $connection = connect();
+        $sql = "DELETE FROM `user_inbox` WHERE `id`= :messageId AND `userReceiveId`=:userId";
+        $sth = $connection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute(array(':messageId' => $messageId, ':userId' => $tokenOwner));
+        //If any row was affected will return success code (1)
+        if($sth->rowCount()!=0){
+            return 1;
+        }else{
+            return "Message does not exist";
+        }
+    }
+    
+}
