c670a1760c86bec1b8715b513a3f6e9d9923e8ca
mas886 <thealchemist886@gmail.com>
2017-02-13 22:44:56 +0100

    Removed "baseStats" column from `monsters`

--- db/DBstructure/gameDB.sql
+++ db/DBstructure/gameDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 13, 2017 at 10:36 PM
+-- Generation Time: Feb 13, 2017 at 10:43 PM
 -- Server version: 10.0.29-MariaDB-0ubuntu0.16.10.1
 -- PHP Version: 7.0.13-0ubuntu0.16.10.1
 
@@ -149,7 +149,6 @@ CREATE TABLE `monsters` (
   `id` int(5) NOT NULL,
   `name` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
   `description` text COLLATE utf8_spanish_ci NOT NULL,
-  `baseStats` text COLLATE utf8_spanish_ci NOT NULL,
   `sprite` varchar(90) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Monster table.';
 
