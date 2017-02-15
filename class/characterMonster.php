34987a50751963c2db7c52f1e2d73bc4cb5caa36
judit09 <judit_ml_96@hotmail.com>
2017-02-15 19:47:55 +0100

    Scheme added of characterMonster class from character_monster table

---
+++ class/characterMonster.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Class to control monster of every character
+ *
+ * @author PATATA
+ */
+class character_monster {
+    public function addMonster(){}
+    public function deleteMonster(){}
+    public function monsterList(){}
+    public function addExp(){}
+    public function getMonster(){}
+
+}
