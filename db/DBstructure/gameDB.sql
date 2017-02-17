304c1a84db0784414b9c4b6ad1c45d9b3e6a5d38
mas886 <thealchemist886@gmail.com>
2017-02-17 18:48:08 +0100

    Added defence column to monster_stats

--- db/DBstructure/gameDB.sql
+++ db/DBstructure/gameDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 17, 2017 at 06:27 PM
+-- Generation Time: Feb 17, 2017 at 06:45 PM
 -- Server version: 10.0.29-MariaDB-0ubuntu0.16.10.1
 -- PHP Version: 7.0.13-0ubuntu0.16.10.1
 
@@ -171,16 +171,17 @@ CREATE TABLE `monster_stats` (
   `accuracy` int(8) NOT NULL,
   `speed` int(8) NOT NULL,
   `strength` int(8) NOT NULL,
-  `vitality` int(8) NOT NULL
+  `vitality` int(8) NOT NULL,
+  `defence` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Monster base stats';
 
 --
 -- Dumping data for table `monster_stats`
 --
 
-INSERT INTO `monster_stats` (`monsterId`, `accuracy`, `speed`, `strength`, `vitality`) VALUES
-(1, 10, 10, 10, 40),
-(2, 12, 8, 11, 36);
+INSERT INTO `monster_stats` (`monsterId`, `accuracy`, `speed`, `strength`, `vitality`, `defence`) VALUES
+(1, 10, 10, 10, 40, 10),
+(2, 12, 8, 11, 36, 9);
 
 -- --------------------------------------------------------
 
