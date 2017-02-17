f724ebc734d3421f52ce92276a8fd71b4a3e721c
tinez09 <judit_ml_96@hotmail.com>
2017-02-17 19:27:46 +0100

    Change && error to || on Character

--- class/Character.php
+++ class/Character.php
@@ -10,7 +10,7 @@ class Character {
 
     function addCharacter($characterName, $token) {
         //Add character to db given token
-        if (strlen($characterName) > 20 && strlen($characterName) < 5 && strlen($token) != 30) {
+        if (strlen($characterName) > 20 || strlen($characterName) < 5 || strlen($token) != 30) {
             return 0;
         }
         $tkn = new Token;
@@ -49,7 +49,7 @@ class Character {
 
     function getCharacter($characterName, $token) {
         //Returns the character's information () given token
-        if (strlen($characterName) > 20 && strlen($characterName) < 1 && $token != 30) {
+        if (strlen($characterName) > 20 || strlen($characterName) < 1 || $token != 30) {
             return 0;
         }
         $tkn = new Token;
@@ -64,7 +64,7 @@ class Character {
 
     function addExp($battleExp, $characterName, $token) {
         //After a battle, experience of character is increased with battleExp
-        if ($token != 30 && !ctype_digit($battleExp) && strlen($characterName) > 12 && strlen($characterName) < 1) {
+        if ($token != 30 || !ctype_digit($battleExp) || strlen($characterName) > 12 || strlen($characterName) < 1) {
             return 0;
         }
         $tkn = new Token;
@@ -78,7 +78,7 @@ class Character {
 
     function selectBuild($buildId, $characterName, $token) {
         //select the build for battle of the character
-        if (strlen($characterName) > 20 && strlen($characterName) < 1 && strlen($token) != 30 && !ctype_digit($buildId)) {
+        if (strlen($characterName) > 20 || strlen($characterName) < 1 || strlen($token) != 30 ||  !ctype_digit($buildId)) {
             return 0;
         }
         $tkn = new Token;
