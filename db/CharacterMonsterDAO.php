1962c6a90733f6bc12449cfe75b3abc2265cc5cc
tinez09 <judit_ml_96@hotmail.com>
2017-02-22 00:16:07 +0100

    Added 'monsterId' on query SELECT on characterMonsterList

--- db/CharacterMonsterDAO.php
+++ db/CharacterMonsterDAO.php
@@ -36,7 +36,7 @@ class CharacterMonsterDAO {
     public function characterMonsterList($characterName) {
         //Return a array if there's some monster and empty array if there's no one
         $connection = connect();
-        $sql = "SELECT `id`,`experience`,`statsModifier` FROM `character_monster` WHERE characterId = (SELECT `id` FROM `user_character` WHERE name = :name)";
+        $sql = "SELECT `monsterId`,`id`,`experience`,`statsModifier` FROM `character_monster` WHERE characterId = (SELECT `id` FROM `user_character` WHERE name = :name)";
         $sth = $connection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
         $sth->execute(array(':name' => $characterName));
         $monsters = $sth->fetchAll(PDO::FETCH_ASSOC);
