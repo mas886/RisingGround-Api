7e5d22ccce1c15614ef4d4792d20e4e409949929
mas886 <thealchemist886@gmail.com>
2017-02-22 11:54:28 +0100

    Added  damage randomizer function.

--- mechanics/Battle.php
+++ mechanics/Battle.php
@@ -10,7 +10,13 @@ class Battle {
     
     private $AccuracySpeedRange=[[-200,100],[1,99]];
     private $AttackDefenceRange=[[-300,300],[0.1,1.9]];
-
+        
+    private function damageRandomizer($damage){
+        //This will apply a random multiplier from 0.8 50 1.2 to the original static calculated damage
+        $damageRandomizer=rand (0.80*100, 1.20*100) / 100;
+        return $damageRandomizer*$damage;
+    }
+    
     private function calculateRange($difference,$range){
         //Formula: low2+(x-low1)*(high2-low2)/(high1-low1)
         $result=$range[1][0]+($difference-$range[0][0])*($range[1][1]-$range[1][0])/($range[0][1]-$range[0][0]);
