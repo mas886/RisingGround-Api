999979de9bdf0c8345509a01866d405dc2a12bea
tinez09 <judit_ml_96@hotmail.com>
2017-02-22 10:30:41 +0100

    Restored config

--- class/config.php
+++ class/config.php
@@ -2,10 +2,10 @@
         
     function connect(){
         
-        $hostdb="localhost";
-        $userdb="root"; 
-        $passdb="";
-        $database="credit";       
+        $hostdb="SERVER";
+        $userdb="USERNAME"; 
+        $passdb="PASSWORD";
+        $database="DATANAME";       
          
         $connection = new PDO("mysql:host=$hostdb;dbname=$database;charset=utf8mb4", "$userdb", "$passdb", array(PDO::ATTR_PERSISTENT=>true))or die ("Couldn't connect to db.");
         
