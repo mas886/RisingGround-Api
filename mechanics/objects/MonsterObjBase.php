bdd0346cb5a99c5e172fdb75483bfe6f3d288082
mas886 <thealchemist886@gmail.com>
2017-02-27 16:36:54 +0100

    Added "MonsterObjBase.php" file to extend "MonsterObj.php" file.

---
+++ mechanics/objects/MonsterObjBase.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Instance this to create a monsterObj type object with base stats
+ * Needs team variable to define the team in the battle flow and monster id
+ * from monster table
+ *
+ * @author mas886/redrednose/arnau
+ */
+
+include_once("./class/Monster.php");
+include_once("./mechanics/objects/MonsterObj.php");
+
+class monsterObjBase extends monsterObj{
+    
+    function __construct($monsterId,$team) {
+        $monster=new Monster;
+        $statsArray=$monster->getMonster($monsterId);
+        parent::__construct($statsArray,$team);
+    }
+
+}
