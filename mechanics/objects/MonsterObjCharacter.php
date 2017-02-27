cc444ffc5b19db6d61250268ed6db31cf44b8612
mas886 <thealchemist886@gmail.com>
2017-02-27 22:06:52 +0100

    Added "MonsterObjCharacter" class to extend "MonsterObj.php."

---
+++ mechanics/objects/MonsterObjCharacter.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Instance this to create a monsterObj type object with character's monster stats
+ * Needs team variable to define the team in the battle flow and character monster id
+ * from character_monster table
+ *
+ * @author mas886/redrednose/arnau
+ */
+
+
+include_once("./class/CharacterMonster.php");
+include_once("./mechanics/objects/MonsterObj.php");
+
+class MonsterObjCharacter extends monsterObj{
+    
+    function __construct($characterMonsterId,$team) {
+        $monster=new CharacterMonster;
+        $statsArray=$monster->getCharacterMonster($characterMonsterId);
+        parent::__construct($statsArray,$team);
+    }
+}
