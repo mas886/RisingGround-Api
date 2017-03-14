d2ea26a2da4f62ccce556a7ee51ff90f0da464e8
mas886 <thealchemist886@gmail.com>
2017-03-14 23:46:42 +0100

    Added initial version of "Dungeon.php".

---
+++ class/Dungeon.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Class to interact with the dungeon system
+ *
+ * @author mas886/redrednose/arnau
+ */
+include_once("./class/Character.php");
+include_once("./db/DungeonDAO.php");
+
+class Dungeon {
+
+    public function getCharacterAvailableDungeons($characterName, $token) {
+        if (strlen($characterName) > 20 || strlen($characterName) < 5 || strlen($token) != 30) {
+            return 0;
+        } else {
+            $tkn = new Token;
+            //Check of user token.
+            $tokenOwner = $tkn->getUserIdByToken($token);
+            if ($tokenOwner == "Expired" || $tokenOwner == "Bad token") {
+                return $tokenOwner;
+            }
+            $char = new Character;
+            //Check character belonging
+            if (!$char->checkOwner($characterName, $tokenOwner)) {
+                return "Wrong Character";
+            }else{
+                //Once we passed everything we proceed to get the available dungeons
+                $dao = new DungeonDAO;
+                return $dao->getCharacterDungeons($characterName);
+            }
+        }
+    }
+
+}
