7f8465f06d0cf8dbf6b4a931d565eb73b6044a2d
mas886 <thealchemist886@gmail.com>
2017-03-15 13:09:24 +0100

    Added "checkCharacterDungeonAccessLvl" function at "DungeonDAO.php".

--- db/DungeonDAO.php
+++ db/DungeonDAO.php
@@ -12,7 +12,7 @@ include_once("./class/Character.php");
 class DungeonDAO {
     
     public function getCharacterDungeons($characterName){
-        //We get user's lvl
+        //We get character's lvl
         $lvl= $this->getCharacterLevel($characterName);
         $connection = connect();
         $sql = "SELECT `id`, `name`, `description`, `minLevel` FROM `dungeon` WHERE`minLevel`<= :minLvl";
@@ -25,6 +25,21 @@ class DungeonDAO {
     private function getCharacterLevel($characterName){
         $char=new Character;
         return $char->getCharacterLevel($characterName);
+    }
+    
+    public function checkCharacterDungeonAccessLvl($dungeonId,$characterName){
+        //This function returns an array with the first dungeon_level's level values if the access is granted.
+        //False is returned when the lvl requirement is not met.
+        //We get character's lvl
+        $level=$this->getCharacterLevel($characterName);
+        $connection = connect();
+        $sql = "SELECT `dungeon_level`.`id`, `dungeon_level`.`dungeonId`, `dungeon`.`name`, `dungeon_level`.`name`, `dungeon_level`.`description`  FROM `dungeon_level` JOIN `dungeon` WHERE `dungeon_level`.`dungeonId` = `dungeon`.`id` AND `dungeon`.`id` = :dungeonId AND `dungeon`.`minLevel`<= :playerLvl LIMIt 1";
+        $sth = $connection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute(array(':playerLvl' => $level,':dungeonId' => $dungeonId));
+        $dungeonData = $sth->fetch(PDO::FETCH_ASSOC);
+        //If there's NO access to the dungeon FALSE is returned
+        //Note: If a dungeon exist but doesn't contain any lvl even if the minLvl requirement is met FALSE will be returned
+        return $dungeonData;
     }
     
 }
