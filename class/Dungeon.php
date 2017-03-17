20f130c45863297d2dc20eb2ca2afde4851b6fbe
mas886 <thealchemist886@gmail.com>
2017-03-17 11:05:57 +0100

    Added public function "listDungeonLevels" at "Dungeon.php".

--- class/Dungeon.php
+++ class/Dungeon.php
@@ -32,9 +32,35 @@ class Dungeon {
         }
     }
 
+    public function listDungeonLevels($characterName, $token, $dungeonId) {
+        //Returns the list of dungeon lvls plus a field indicating it's availability towards the player
+        if (strlen($characterName) > 20 || strlen($characterName) < 5 || strlen($token) != 30 || !is_numeric($dungeonId)) {
+            return 0;
+        } else {
+            $tkn = new Token;
+            //Check of user token.
+            $tokenOwner = $tkn->getUserIdByToken($token);
+            if ($tokenOwner == "Expired" || $tokenOwner == "Bad token") {
+                return $tokenOwner;
+            }
+            $char = new Character;
+            //Check character belonging
+            if (!$char->checkOwner($characterName, $tokenOwner)) {
+                return "Wrong Character";
+            }
+            //We check dungeon access, if there are no lvls on a dungeon access will be dennied
+            if (!$this->checkCharacterDungeonAccess($characterName, $dungeonId)) {
+                return "Access Dennied";
+            }
+            //Once we passed everything we proceed to get the available dungeons
+            $dao=new DungeonDAO;
+            return $dao->listDungeonLvls($dungeonId, $characterName);
+        }
+    }
+
     private function checkCharacterDungeonAccess($characterName, $dungeonId) {
         //On this function we follow a sequence of checks, once all are passed "true"is returned 
-        if(!$this->levelAccessCheck($characterName, $dungeonId)){
+        if (!$this->levelAccessCheck($characterName, $dungeonId)) {
             return false;
         }
         return true;
@@ -42,10 +68,10 @@ class Dungeon {
 
     private function levelAccessCheck($characterName, $dungeonId) {
         $dao = new DungeonDAO;
-        if($dao->checkCharacterDungeonAccessLvl($dungeonId, $characterName)==FALSE){
+        if ($dao->checkCharacterDungeonAccessLvl($dungeonId, $characterName) == FALSE) {
             //If the query results is FALSE means the user have no access to the dungeon
             return false;
-        }else{
+        } else {
             return true;
         }
     }
