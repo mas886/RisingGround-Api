647c276a07ea382e519e844b21e8d8dd625e94f7
mas886 <thealchemist886@gmail.com>
2017-03-25 13:24:34 +0100

    Now getLevelStages returns client-friendly data.

--- mechanics/DungeonSys.php
+++ mechanics/DungeonSys.php
@@ -6,13 +6,19 @@
  * @author mas886/redrednose/arnau
  */
 include_once("./db/DungeonDAO.php");
+include_once("./mechanics/objects/DungeonStageObj.php");
 
 class DungeonSys {
 
     public function getLevelStages($levelId, $characterName) {
         $dao = new DungeonDAO;
         $stages = $dao->listCharacterDungeonLevelStages($levelId, $characterName);
-        return $stages;
+        //We convert what we get from the DB to usefull data for the client
+        foreach ($stages as $stage){
+            $dunObj=new DungeonStageObj($stage);
+            $stagesRet[]=$dunObj->getStage();
+        }
+        return $stagesRet;
     }
 
 }
