8107c84821ca5360e6f14393a75686e280a05ceb
mas886 <thealchemist886@gmail.com>
2017-04-15 23:43:16 +0200

    Added "claimReward" function.

--- class/Reward.php
+++ class/Reward.php
@@ -6,6 +6,7 @@
  * @author mas886/redrednose/arnau
  */
 include_once("./mechanics/RewardSys.php");
+include_once("./mechanics/objects/DungeonStageObj.php");
 include_once("./db/RewardDAO.php");
 include_once ("Character.php");
 
@@ -27,7 +28,35 @@ class Reward {
             $dao=new RewardDAO;
             return $dao->listAvailableRewards($characterName);
         }
-        
+    }
+    
+    public function claimReward($characterName,$token,$rewardId){
+        if (strlen($characterName) > 20 || strlen($characterName) < 1 || strlen($token) != 30 || !is_numeric($rewardId)) {
+            return 0;
+        }
+        $tkn = new Token;
+        $userId = $tkn->getUserIdByToken($token);
+        if ($userId == "Expired" || $userId == "Bad token") {
+            return $userId;
+        }
+        if(!Character::checkOwner($characterName, $userId)){
+            return ("Wrong character");
+        }
+        $dao=new RewardDAO;
+        $reward= $dao->getCharacterRewardWhenAvailable($characterName, $rewardId);
+        if($reward!=NULL){
+            if ($reward['stageCompletedId']!=NULL){
+                $stage=new DungeonStageObj($reward['stageCompletedId']);
+                $this->applyReward($characterName, $stage);
+            }
+            if($reward['reward']!=NULL){
+                $this->applyRewardString($characterName, $reward['reward']);
+            }
+            return 1;
+        }
+        return "Reward does not exist";
+    }
+    
     public function applyRewardString($characterName, $reward){
         $rew=new RewardSys;
         return $this->applyRewardString($characterName, $reward);
@@ -44,5 +73,4 @@ class Reward {
         return $dao->stashStageReward($characterName, $stageId, $visibleAfter);
     }
     
-}
 }
