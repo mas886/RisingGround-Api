71d37b5b90a320e8cbefbab3c65b8c3fa7a1b22a
tinez09 <judit_ml_96@hotmail.com>
2017-05-22 12:29:13 +0200

    Correct SQL from characterMonsterList()

--- db/CharacterMonsterDAO.php
+++ db/CharacterMonsterDAO.php
@@ -41,7 +41,7 @@ class CharacterMonsterDAO {
     public function characterMonsterList($characterName) {
         //Return a array if there's some monster and empty array if there's no one
         $connection = connect();
-        $sql = "SELECT `monsterId`,`id`,`experience` FROM `character_monster` WHERE characterId = (SELECT `id` FROM `user_character` WHERE name = :name)";
+        $sql = "SELECT monster.name, monster.description, monster.sprite, character_monster.monsterId, character_monster.id, character_monster.experience, character_monster.buildId FROM `character_monster` JOIN monster WHERE characterId = (SELECT `id` FROM `user_character` WHERE name = :name) AND monster.id = character_monster.monsterId";
         $sth = $connection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
         $sth->execute(array(':name' => $characterName));
         $monsters = $sth->fetchAll(PDO::FETCH_ASSOC);
